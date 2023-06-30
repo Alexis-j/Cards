6123834b088acf889b6ff1fb92c1b6486b69bbde
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-06-30 10:52:28 +0200

    prep fot the main & app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { Container } from "@mui/material";
+import Pizza from "./components/Pizza";
+import Patineta from "./components/Patineta";
+import Postcard from "./components/Postcard";
 
+export default function App() {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Pizza />
+      <Patineta />
+      <Postcard />
+    </Container>
+  );
